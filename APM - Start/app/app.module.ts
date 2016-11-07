605ea8bc4baf96ab35af919aa39580264faa9c93
Memfis <nikitamemfis@gmail.com>
2016-11-07 23:56:50 +0400

    finish services and http modules

--- APM - Start/app/app.module.ts
+++ APM - Start/app/app.module.ts
@@ -1,14 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import ProductListComponent  from './features/product-list/product-list.component';
-import {ProductFilterPipe} from './features/product-list/product-filter.pipe';
+import {ProductFilterPipe} from './features/product-list/pipes/product-filter.pipe';
 import {StarRatingComponent} from './shared/components/star-rating/star-rating.component';
 
 @NgModule({
-  imports: [ BrowserModule, FormsModule ],
+  imports: [ BrowserModule, FormsModule, HttpModule ],
   declarations: [ 
   	AppComponent,
    	ProductListComponent,
--- APM - Start/app/features/product-list/product-list.component.ts
+++ APM - Start/app/features/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import {IProduct} from './product.interface';
+import {IProduct} from './interfaces/product.interface';
+import {ProductService} from './services/product.service';
 
 @Component({
 	selector: 'pm-products',
@@ -12,37 +13,23 @@ export default class ProductListComponent implements OnInit {
 	imageWidth: number = 50;
 	imageMargin: number = 2;
 	isImageShowing: boolean = false;
-	listFilter: string = 'cart';
-	products: IProduct[] = [{
-      "productId": 1,
-      "productName": "Leaf Rake",
-      "productCode": "GDN-0011",
-      "releaseDate": "March 19, 2016",
-      "description": "Leaf rake with 48-inch wooden handle.",
-      "price": 19.95,
-      "starRating": 3.2,
-      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-    },
-    {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2016",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-    }];
+	listFilter: string;
+  errorMessage: string;
+  products: IProduct[];
+	
+  constructor(private _productService: ProductService) {}
 
-    toggleImage(): void {
-    	this.isImageShowing = !this.isImageShowing;
-    }
+  toggleImage(): void {
+  	this.isImageShowing = !this.isImageShowing;
+  }
 
-    ngOnInit(): void {
-    	console.log('initialized');
-    }
+  ngOnInit(): void {
+  	this._productService.getProducts()
+      .subscribe(products => this.products = products,
+                 error => this.errorMessage = <any>error);
+  }
 
-    onNotify(msg: string): void {
-      this.pageTitle = msg;
-    }
+  onNotify(msg: string): void {
+    this.pageTitle = msg;
+  }
 }
